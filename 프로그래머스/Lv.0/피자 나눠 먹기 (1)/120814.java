5db0ef533b0040893cb63e7c94b7ccc49706772c
Eunho Cho <ehcho1231@gmail.com>
2025-03-05 18:56:23 +0900

    Create 120814.java

---
+++ 프로그래머스/Lv.0/피자 나눠 먹기 (1)/120814.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(int n) {
+        int answer = 1;
+
+        while (n > 7) {
+            n -= 7;
+            answer++;
+        }
+
+        return answer;
+    }
+}
