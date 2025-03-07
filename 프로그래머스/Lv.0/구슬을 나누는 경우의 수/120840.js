b60836665294054eb0697e93c811d2ef6785f570
Eunho Cho <ehcho1231@gmail.com>
2025-03-07 21:58:47 +0900

    Create 120840.js

---
+++ 프로그래머스/Lv.0/구슬을 나누는 경우의 수/120840.js
@@ -0,0 +1,10 @@
+function solution(balls, share) {
+    let answer = 1;
+    
+    for (let i=1; i<=share; i++) {
+        answer *= (balls - i + 1);
+        answer /= i;
+    }
+    
+    return answer;
+}
