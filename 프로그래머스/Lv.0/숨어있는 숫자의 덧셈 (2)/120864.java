de06366cf7a201f20fe1c9c7514a5f76356e52c3
Eunho Cho <ehcho1231@gmail.com>
2025-03-09 22:41:16 +0900

    Create 120864.java

---
+++ 프로그래머스/Lv.0/숨어있는 숫자의 덧셈 (2)/120864.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int solution(String my_string) {
+        int answer = 0;
+        
+        my_string = my_string.replaceAll("[^0-9]", ",");
+        for (String s : my_string.split(",")) {
+            if (!"".equals(s)) answer += Integer.parseInt(s);
+        }
+        
+        return answer;
+    }
+}
